855238f776747245081c70d95ba0ded990f87f1a
Manas Kelshikar <manas@stackstorm.com>
2014-07-11 13:21:13 -0700

    Better understanding of statics.

--- classes/teststatics.py
+++ classes/teststatics.py
@@ -1,5 +1,5 @@
 class TestStatics(object):
-    static_1 = None 
+    static_1 = None
     static_2 = 'ClassStatic-2'
     static_3 = 'ClassStatic-3'
 
@@ -10,14 +10,27 @@ class TestStatics(object):
     def __str__(self):
         return 'static_1 : {}, static_2 : {}, static_3 : {}'.format(self.static_1, self.static_2, self.static_3)
 
+    @classmethod
+    def update_class_statics(cls, update_value='ClassStatic-3-updated'):
+        cls.static_3 = update_value
+
     def print_class_statics(self):
         print 'static_1 : {}, static_2 : {}, static_3 : {}'.format(TestStatics.static_1, TestStatics.static_2, TestStatics.static_3)
 
 
 if __name__ == '__main__':
+    print '===== ts1 ====='
     ts1 = TestStatics('ts1_v1','ts1_v2')
-    ts2 = TestStatics('ts2_v1','ts2_v2')
     print ts1
-    print ts2
     ts1.print_class_statics()
-    ts2.print_class_statics() 
+
+    print '===== ts2-1 ====='
+    TestStatics.update_class_statics()
+    ts2 = TestStatics('ts2_v1','ts2_v2')
+    print ts2
+    ts2.print_class_statics()
+
+    print '===== ts2-2 ====='
+    TestStatics.update_class_statics('ClassStatic-3-updated-2')
+    print ts2
+    ts2.print_class_statics()
